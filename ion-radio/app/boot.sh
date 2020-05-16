b81a06ea46e8e07637cad440630674528826c9d6
Aaria Carter-Weir <aaria@mxstore.com.au>
2020-05-16 14:36:05 +1000

    tweaking to boot config files on the fly

--- ion-radio/app/boot.sh
+++ ion-radio/app/boot.sh
@@ -1,9 +1,15 @@
 #!/bin/sh
 
+# make boot directory
+mkdir boot
+
 # START BOTS
 sh ./start-bot.sh -v 1 -t "$BOT_TOKEN_1"
 sh ./start-bot.sh -v 2 -t "$BOT_TOKEN_2"
 sh ./start-bot.sh -v 3 -t "$BOT_TOKEN_3"
+
+# clean boot directory
+rmdir boot
 
 # keep alive
 trap : TERM INT; sleep infinity & wait
--- ion-radio/app/start-bot.sh
+++ ion-radio/app/start-bot.sh
@@ -9,11 +9,19 @@ do
     esac
 done
 
+cp ./config.txt boot/config.txt
+
 # add bot token
-sed -i "s/\[BOT_TOKEN\]/$TOKEN/" "bot-${VERSION}/config.txt"
+sed -i "s/\[BOT_TOKEN\]/$TOKEN/" "boot/config.txt"
 
 # update owner id
-sed -i "s/\[OWNER_ID\]/$OWNER_ID/" "bot-${VERSION}/config.txt"
+sed -i "s/\[OWNER_ID\]/$OWNER_ID/" "boot/config.txt"
+
+# update prefix
+sed -i "s/\[PREFIX\]/\-${VERSION}/" "boot/config.txt"
 
 # boot the bot!
-cd "bot-${VERSION}" && nohup java -Dnogui=true -jar /app/music-bot.jar &
+#cd boot && nohup java -Dnogui=true -jar /app/music-bot.jar &
+
+# clear up boot
+rm boot/config.txt
