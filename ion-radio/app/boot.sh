7a5002940453f7b3278fac81eed0143c7ae46d88
Aaria Carter-Weir <aaria@mxstore.com.au>
2020-05-16 14:29:45 +1000

    getting ready to support up to three bots

--- ion-radio/app/boot.sh
+++ ion-radio/app/boot.sh
@@ -1,7 +1,9 @@
 #!/bin/sh
 
-## BOT 1
+# START BOTS
 sh ./start-bot.sh -v 1 -t "$BOT_TOKEN_1"
+sh ./start-bot.sh -v 2 -t "$BOT_TOKEN_2"
+sh ./start-bot.sh -v 3 -t "$BOT_TOKEN_3"
 
 # keep alive
 trap : TERM INT; sleep infinity & wait
