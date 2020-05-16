942f37cbaa2eb97803991eecdc1d1f612fe1377b
Aaria Carter-Weir <aaria@mxstore.com.au>
2020-05-16 15:56:16 +1000

    try just one bot

--- app/boot.sh
+++ app/boot.sh
@@ -5,10 +5,10 @@ mkdir boot
 
 # START BOTS
 sh ./start-bot.sh -v 1 -t "$BOT_TOKEN_1"
-sh ./start-bot.sh -v 2 -t "$BOT_TOKEN_2"
-sh ./start-bot.sh -v 3 -t "$BOT_TOKEN_3"
-sh ./start-bot.sh -v 4 -t "$BOT_TOKEN_4"
-sh ./start-bot.sh -v 5 -t "$BOT_TOKEN_5"
+#sh ./start-bot.sh -v 2 -t "$BOT_TOKEN_2"
+#sh ./start-bot.sh -v 3 -t "$BOT_TOKEN_3"
+#sh ./start-bot.sh -v 4 -t "$BOT_TOKEN_4"
+#sh ./start-bot.sh -v 5 -t "$BOT_TOKEN_5"
 
 # keep alive
 trap : TERM INT; sleep infinity & wait
