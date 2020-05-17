969864229843e52c6a1cf17ba04c86ce866b8184
Aaria Carter-Weir <aaria@mxstore.com.au>
2020-05-17 11:06:01 +1000

    remove formatting

--- bots.sh
+++ bots.sh
@@ -1,31 +1,38 @@
 #!/bin/sh
 
 # PARSE ARGS
-while getopts fa: option
+while getopts fa:v: option
 do
     case "${option}" in
         f) FORCE='true';;
         a) ACTION=${OPTARG};;
+        v) VERSION=${OPTARG};;
     esac
 done
 
 if [ ! "$ACTION" ]; then
-  echo "\x1b[35m""Action must be provided, use the -a flag. Available actions are:"
-  echo "    \x1b[95m""boot\x1b[35m - Starts the bots up from scratch if there is no pre-existing container"
-  echo "    \x1b[95m""suspend\x1b[35m - Pauses the bots"
-  echo "    \x1b[95m""resume\x1b[35m - Resumes paused bots"
-  echo "    \x1b[95m""stop\x1b[35m - Shuts down the bot containers"
-  echo "    \x1b[95m""start\x1b[35m - Starts up the bot containers"
-  echo "use the \x1b[95m-f\x1b[35m flag to run \x1b[91m""docker system prune --force"
+  echo "Action must be provided, use the -a flag. Available actions are:"
+  echo "    boot - Starts the bots up from scratch if there is no pre-existing container"
+  echo "    suspend - Pauses the bots"
+  echo "    resume - Resumes paused bots"
+  echo "    stop - Shuts down the bot containers"
+  echo "    start - Starts up the bot containers"
+  echo "use the -f flag to run docker system prune --force"
   exit 2
 fi
 
-sh "util/${ACTION}-bot.sh" -v 1
-sh "util/${ACTION}-bot.sh" -v 2
-sh "util/${ACTION}-bot.sh" -v 3
-sh "util/${ACTION}-bot.sh" -v 4
-sh "util/${ACTION}-bot.sh" -v 5
-sh "util/${ACTION}-bot.sh" -v 6
+if [ ! "$VERSION" ]; then
+  echo "Running action '${ACTION}' for all bots"
+  sh "util/${ACTION}-bot.sh" -v 1
+  sh "util/${ACTION}-bot.sh" -v 2
+  sh "util/${ACTION}-bot.sh" -v 3
+  sh "util/${ACTION}-bot.sh" -v 4
+  sh "util/${ACTION}-bot.sh" -v 5
+  sh "util/${ACTION}-bot.sh" -v 6
+else
+  echo "Running action '${ACTION}' for bot version '${VERSION}'"
+  sh "util/${ACTION}-bot.sh" -v "$VERSION"
+fi
 
 if [ "$FORCE" ]; then
   docker system prune --force
