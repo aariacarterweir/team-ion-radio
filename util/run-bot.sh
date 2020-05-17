5c0f8bb4c0065b3194c7ba7a2619447e617dba56
Aaria Carter-Weir <aaria@mxstore.com.au>
2020-05-17 12:01:51 +1000

    yknow what? non dot name is fine

--- util/run-bot.sh
+++ util/run-bot.sh
@@ -9,16 +9,16 @@ do
 done
 
 # create persist directory if it does not exist
-if [ ! -d ".persist/${VERSION}" ]; then
-  mkdir ".persist/${VERSION}"
-  chmod -R 755 ".persist/${VERSION}"
+if [ ! -d "persist/${VERSION}" ]; then
+  mkdir "persist/${VERSION}"
+  chmod -R 755 "persist/${VERSION}"
 fi
 
 # docker run command
 docker run \
   --detach \
   --tty \
-  --mount  type=bind,source="$(pwd)/.persist/${VERSION}",target=/app/persist \
+  --mount  type=bind,source="$(pwd)/persist/${VERSION}",target=/app/persist \
   --env VERSION="$VERSION" \
   --env-file .env \
   --name "ion-radio-${VERSION}" \
