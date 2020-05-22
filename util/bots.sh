6a62cf22f946600e9827f0a91ac856f6c12f4d3a
Aaria Carter-Weir <aaria@mxstore.com.au>
2020-05-22 22:51:52 +1000

    try using word split instead

--- util/bots.sh
+++ util/bots.sh
@@ -31,7 +31,8 @@ fi
 # Run for either all versions or a specific version
 if [ ! "$VERSION" ]; then
   echo "Running action '${ACTION}' for all bots"
-  for v in "${BOTS[@]}"; do
+  setopt shwordsplit
+  for v in $BOTS; do
     sh "util/bot-${ACTION}.sh" -v "$v"
   done
 else
